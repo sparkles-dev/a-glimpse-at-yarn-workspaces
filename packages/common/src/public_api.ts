22e84230efaee7f2b3606fa3a7faf23ba8d6490c
David <david@spektrakel.de>
2017-07-30 08:30:37 +0200

    feat: demonstrate ng-packagr in a monorepo

---
+++ packages/common/src/public_api.ts
@@ -0,0 +1,3 @@
+import { Version } from '@angular/core';
+
+export const FOO: Version = new Version('1.0.0-alpha.13');
---
+++ packages/components/src/public_api.ts
@@ -0,0 +1,3 @@
+import { Version } from '@angular/core';
+
+export const BAR: Version = new Version('1.0.0');
